32adeacdb7d41523858c7cdd9ae5c6dd1be29822
Jakub <83830495+JKBVSKY@users.noreply.github.com>
2025-05-28 20:19:06 +0200

    PWA init

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/JMP-Tools/', // Replace <repository-name> with your GitHub repository name
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'JMP Tools',
+        short_name: 'JMPTools',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
